c88eca2fd55e14e9b23a7dcf20f5007729d2633b
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2023-08-17 19:22:21 +0300

    add the properties section

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -6,7 +6,6 @@ interface IParams {
 }
 
 export async function DELETE(request: Request, { params }: { params: IParams}) {
-  
     const currentUser = await getCurrentUser();
     if (!currentUser) {
       return NextResponse.error();
